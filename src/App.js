9b598716372019184a7a361b871e7ee73b4fcccf
aryelciu001 <aryelciu001@gmail.com>
2020-03-26 00:21:51 +0800

    Added query result page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 //importing components
 import HomePage from "./Components/HomePage";
+import QueryResult from "./Components/QueryResult";
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <QueryResult></QueryResult>
     </div>
   );
 }
---
+++ src/Components/QueryResult/QueryResult1/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+import Button from "../../Button";
+
+import "./index.css";
+
+class QueryResult1 extends Component {
+  componentDidMount() {
+    console.log(this.props.res.hotel.bg);
+  }
+  render() {
+    const style = {
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat"
+    };
+    const styleHotel = {
+      background: `url(${this.props.res.hotel.bg})`
+    };
+    const styleFlight = {
+      background: `url(${this.props.res.flight.logo})`
+    };
+    const styleItenerary = {
+      background: `url(${this.props.res.itenerary.logo})`
+    };
+
+    return (
+      <div className="qr-1">
+        <div className="qr-11 qr-1hotel">
+          <div className="qr-bg" style={{ ...styleHotel, ...style }}></div>
+          <div className="qr-inf">
+            {this.props.res.hotel.name}
+            <br></br>
+            {this.props.res.hotel.price}
+          </div>
+        </div>
+        <div className="qr-11 qr-1flight">
+          <div className="qr-bg" style={{ ...styleFlight, ...style }}></div>
+          <div className="qr-inf">
+            {this.props.res.flight.name}
+            <br />
+            {this.props.res.flight.price}
+          </div>
+        </div>
+        <div className="qr-11 qr-1amenities">
+          <div className="qr-bg" style={{ ...styleItenerary, ...style }}></div>
+          <div className="qr-inf">
+            {this.props.res.itenerary.name}
+            <br />
+            {this.props.res.itenerary.price}
+          </div>
+        </div>
+        <div className="info-btn">
+          <Button text="Details"></Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QueryResult1;
