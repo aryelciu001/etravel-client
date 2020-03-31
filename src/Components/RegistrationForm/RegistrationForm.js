9b97cdcc9eb710d2314addcb115f8b0c1c46f78c
Aldo Halim <aldohalim28@gmail.com>
2020-03-31 15:49:37 +0800

    Connect register with backend

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -3,29 +3,88 @@ import "./RegistrationForm.css";
 import Button from "../Button";
 
 class RegistrationForm extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    country: "",
+    phoneNumber: ""
+  };
+  register = () => {
+    const axios = require("axios");
+    const url = "http://localhost:5000/profiles/add";
+    const { name, email, password, country, phoneNumber } = this.state;
+    axios
+      .post(url, { name, email, password, country, phoneNumber })
+      .then(res => {
+        if (res.data.err) {
+          this.setState({
+            ...this.state,
+            feedback: "User exists! Please login"
+          });
+        } else {
+          window.location.replace("/profilingtest");
+        }
+      });
+  };
+
+  onChange = e => {
+    this.setState({
+      ...this.state,
+      [e.target.id]: e.target.value,
+      feedback: ""
+    });
+  };
+
   render() {
+    const { name, email, password, country, phoneNumber } = this.state;
     return (
       <form className="login-form">
         <ul>
           <li>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={this.onChange}
+            />
           </li>
           <li>
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" />
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={this.onChange}
+            />
           </li>
           <li>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={this.onChange}
+            />
           </li>
           <li>
             <label htmlFor="country">Country</label>
-            <input type="text" id="country" />
+            <input
+              type="text"
+              id="country"
+              value={country}
+              onChange={this.onChange}
+            />
           </li>
           <li>
             <label htmlFor="phone">Phone Number</label>
-            <input type="text" id="phone" />
+            <input
+              type="text"
+              id="phoneNumber"
+              value={phoneNumber}
+              onChange={this.onChange}
+            />
           </li>
         </ul>
         <div className="submit-wrapper">
